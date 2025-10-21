a6e1d881316d50561f7b3f4e1b2472ac876e8b42
Walter Barcellos Pereira Neto <wbpnok@gmail.com>
2025-10-21 23:19:05 +0100

    normalizeName method added
    Method normalizeName does:
    - trims the inputed name;
    - if the input is null, the string will be empty;
    - Adjust the input to lower case and then make sure that just the first letter will be upper case.

--- src/model/FoodItem.java
+++ src/model/FoodItem.java
@@ -8,5 +8,12 @@ public class FoodItem {
     private String name;
     private double weightGrams;
     
+    private static String normalizeName(String nameInputed) {
+    if (nameInputed == null) return "";
+    nameInputed = nameInputed.trim();
+    if (nameInputed.isEmpty()) return "";
+    String lower = nameInputed.toLowerCase();
+    return Character.toUpperCase(lower.charAt(0)) + lower.substring(1);
+    }
  
 }
