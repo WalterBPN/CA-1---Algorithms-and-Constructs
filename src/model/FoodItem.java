a89585c16e6b76a83ca87ce10fe461bcecab16d9
Walter Barcellos Pereira Neto <wbpnok@gmail.com>
2025-10-21 23:09:41 +0100

    Allowed foods added
    Class FoodItem.java updated

--- src/model/FoodItem.java
+++ src/model/FoodItem.java
@@ -2,5 +2,11 @@
 package model;
 
 public class FoodItem {
+    
+    public static final String[] ALLOWED = { "Burger", "Pizza", "Fries", "Sandwich", "Hotdog" };
 
+    private String name;
+    private double weightGrams;
+    
+ 
 }
