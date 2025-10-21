ffda3ee1fb12cf3a3713d002d21e9e7000650294
Walter Barcellos Pereira Neto <wbpnok@gmail.com>
2025-10-21 23:46:01 +0100

    FoodItem.java updated
    Constructor added;
    Comments added to explain the methods;

--- src/model/FoodItem.java
+++ src/model/FoodItem.java
@@ -8,12 +8,38 @@ public class FoodItem {
     private String name;
     private double weightGrams;
     
-    private static String normalizeName(String nameInputed) {
-    if (nameInputed == null) return "";
-    nameInputed = nameInputed.trim();
-    if (nameInputed.isEmpty()) return "";
-    String lower = nameInputed.toLowerCase();
+    //Constructor
+    public FoodItem(String name, double weightGrams) {
+        String normalized = normalizeName(name); 
+
+        this.name = normalized;
+        this.weightGrams = weightGrams;
+    }
+    
+    /*
+        Trims the input;
+        Returns an empty string if the input is null or blank;
+        Ensures that the string has the first character uppercase and the rest lowercase.
+    */
+    private static String normalizeName(String nameInput) {
+    if (nameInput == null) return "";
+    nameInput = nameInput.trim();
+    if (nameInput.isEmpty()) return "";
+    String lower = nameInput.toLowerCase();
     return Character.toUpperCase(lower.charAt(0)) + lower.substring(1);
     }
+    
+    
+    /*
+        Validates if the food name is in the allowed list;
+        Returns true if the name is valid, otherwise false.
+    */
+    private static boolean isAllowed(String normalized) {
+        for (String s : ALLOWED) {
+            if (s.equals(normalized)) return true;
+        }
+        return false;
+    }
+
  
 }
